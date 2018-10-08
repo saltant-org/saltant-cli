4d813fbbd41fab6f9575504e601525dc9734e406
Matt Wiens <mwiens91@gmail.com>
2018-10-07 17:40:09 -0700

    Finish command to list users

--- saltant_cli/subcommands/users.py
+++ saltant_cli/subcommands/users.py
@@ -3,7 +3,9 @@
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
+import json
 import click
+from tabulate import tabulate
 
 
 @click.group()
@@ -14,12 +16,39 @@ def users(ctx):
 
 
 @users.command()
+@click.option(
+    '--filters',
+    help="Filter keys and values encoded in JSON.",
+    default=None,
+    type=click.STRING,)
+@click.option(
+    '--filters-file',
+    help="Filter keys and values encoded in a JSON file.",
+    default=None,
+    type=click.Path(),)
 @click.pass_context
-def list(ctx):
+def list(ctx, filters, filters_file):
     """List users matching filter parameters."""
     # Get the client from the context
     client = ctx.obj['client']
 
-    user_list = client.users.list()
+    # Build up JSON filters to use
+    if filters is not None:
+        filters = json.loads(filters)
+    else:
+        filters = {}
 
-    print(user_list)
+    if filters_file is not None:
+        with open(filters_file) as f:
+            filters.update(json.load(f))
+
+    # Query for users
+    user_list = client.users.list(filters)
+
+    # Output a pretty table
+    output = tabulate(
+        [[user.username, user.email] for user in user_list],
+        headers=['username', 'email'],
+    )
+
+    click.echo_via_pager(output)
