624133a539825770e1c319e03feb0cdad6194752
Matt Wiens <mwiens91@gmail.com>
2018-10-07 22:14:34 -0700

    Spell Click package name properly in setup.py
    I think it was picky about case.

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
     },
     python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*',
     install_requires=[
-        'click',
+        'Click',
         'PyYAML',
         'saltant-py',
         'tabulate',
