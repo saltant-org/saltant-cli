26a3f5eed72e8871e80e706c3953bd3211c58c3a
Matt Wiens <mwiens91@gmail.com>
2018-10-07 17:53:10 -0700

    Finish user subcommands

--- saltant_cli/subcommands/users.py
+++ saltant_cli/subcommands/users.py
@@ -5,6 +5,7 @@ from __future__ import division
 from __future__ import print_function
 import json
 import click
+from saltant.exceptions import BadHttpRequestError
 from tabulate import tabulate
 
 
@@ -13,6 +14,32 @@ from tabulate import tabulate
 def users(ctx):
     """Command group to interface with users."""
     pass
+
+
+@users.command()
+@click.argument(
+    'username',
+    nargs=1,
+    type=click.STRING,)
+@click.pass_context
+def get(ctx, username):
+    """Get user based on username."""
+    # Get the client from the context
+    client = ctx.obj['client']
+
+    # Query for the user
+    try:
+        user = client.users.get(username)
+
+        # Output a pretty table
+        output = tabulate(
+            [[user.username, user.email]],
+            headers=['username', 'email'],
+        )
+    except BadHttpRequestError:
+        output = "%s not found" % username
+
+    click.echo_via_pager(output)
 
 
 @users.command()
